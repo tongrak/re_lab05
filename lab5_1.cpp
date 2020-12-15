b8624f015c5ffdb5d8adcf4dec23a14edc1e4a06
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2020-12-15 16:11:54 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x = 1, EveNum = 0, OddNum = 0; 
+    while ( x != 0){
+        cout << "Enter an integer: ";
+        cin >> x;
+        if(x > 0 && x!=0){
+            if (x%2 == 0){EveNum++;}
+            else {OddNum++;} 
+        }
+    }
+
+    cout << "#Even numbers = " << EveNum <<endl;
+    cout << "#Odd numbers = " << OddNum;
     return 0;
 }
